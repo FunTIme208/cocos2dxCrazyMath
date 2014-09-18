12ccbae449f37e2d114797fa9b5a0a38b79fcd61
ChenMing <chenming900809@live.cn>
2014-09-18 17:01:15 +0800

    Userdata added

--- Classes/Userdata.cpp
+++ Classes/Userdata.cpp
@@ -2,8 +2,23 @@
 //  Userdata.cpp
 //  CrazyMath
 //
-//  Created by ChenMing on 14-9-16.
+//  Created by ChenMing on 14-9-18.
 //
 //
 
 #include "Userdata.h"
+
+static Userdata *s_SharedUserdata = NULL;
+
+Userdata *Userdata::sharedUserdata()
+{
+    if (!s_SharedUserdata) {
+        s_SharedUserdata = new Userdata;
+    }
+    return s_SharedUserdata;
+}
+
+Userdata::~Userdata()
+{
+    s_SharedUserdata = NULL;
+}
--- Classes/Userdata.h
+++ Classes/Userdata.h
@@ -2,7 +2,7 @@
 //  Userdata.h
 //  CrazyMath
 //
-//  Created by ChenMing on 14-9-16.
+//  Created by ChenMing on 14-9-18.
 //
 //
 
@@ -10,5 +10,21 @@
 #define __CrazyMath__Userdata__
 
 #include <iostream>
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Userdata :public CCObject
+{
+public:
+    static Userdata *sharedUserdata();
+    virtual bool init();
+    virtual ~Userdata();
+    
+    int modeId;
+    
+    static const char MODE_START = 0;
+    static const char MODE_SPECIFICATION = 1;
+    static const char MODE_PRODUCER = 2;
+};
 
 #endif /* defined(__CrazyMath__Userdata__) */
